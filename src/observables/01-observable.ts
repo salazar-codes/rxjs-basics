acb70e52e062bab422a8b2f8adb2aa5347817069
Jimmy Salazar <jimmy.salazar@tektonlabs.com>
2024-01-11 01:54:00 -0500

    unused code removed

--- src/observables/01-observable.ts
+++ src/observables/01-observable.ts
@@ -1,19 +1,18 @@
 import { Observable, Observer } from "rxjs";
 
 const observer: Observer<any> = {
- next: (valor) => console.log(valor),
- error: (error) => console.warn(error),
- complete: () => console.info("Completado"),
+	next: (valor) => console.log(valor),
+	error: (error) => console.warn(error),
+	complete: () => console.info("Completado"),
 };
 
-//const obs$ = Observable.create();
 // Observable de tipo string (fluirán datos string)
 const obs$ = new Observable<string>((subs) => {
- subs.next("Hola");
- subs.next("Hola");
- subs.next("Hola1");
- subs.complete(); //le dice a los subscribers q no emitirás nada más.
- subs.next("Hola2");
+	subs.next("Hola");
+	subs.next("Hola");
+	subs.next("Hola1");
+	subs.complete(); //le dice a los subscribers q no emitirás nada más.
+	subs.next("Hola2");
 });
 
 // obs$.subscribe({
